b32fdc10b8dc0d278ff0e9af45a6d151bbc7735c
khushi-7488 <khushiitiwari88@gmail.com>
2025-09-27 12:12:28 +0530

    React

---
+++ React/basic_react_app/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css"
+
+function Title(){
+  return (
+  <h1>Title</h1>
+  );
+}
+
+function App() {
+  return (
+    <div>
+
+      <h1>this is my h1</h1>
+    <Title/>
+    </div>
+  )
+}
+
+export default App
