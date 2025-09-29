05affb1e7cd35923659c85d16a866fb102eb44b0
khushi-7488 <khushiitiwari88@gmail.com>
2025-09-29 20:09:23 +0530

    practice

--- React/basic_react_app/src/Product.jsx
+++ React/basic_react_app/src/Product.jsx
@@ -2,12 +2,13 @@ import "./Product.css";
 
 function Product({title, price, feature}){
     // const list = feature.map((feature)=> <li>{feature}</li>)
-    let styles = {backgroundColor : price>40000? "yellow" : ""};
+    let isDiscount = price > 30000;
+    let styles = {backgroundColor : isDiscount ? "yellow" : ""};
     return(
     <div className="Product" style={styles}>
     <h3>{title}</h3>
     <h5>{price}</h5>
-    {price>30000 && <p>Discount of 5%</p>}
+    {isDiscount && <p>Discount of 5%</p>}
     </div>
     )
 }
