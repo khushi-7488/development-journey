751c837a7ad3ab92c7b3d5cf014e97a3fee60092
khushi-7488 <khushiitiwari88@gmail.com>
2025-10-03 22:15:29 +0530

    Todo

--- React/Ludu/src/TodoList.jsx
+++ React/Ludu/src/TodoList.jsx
@@ -1,20 +1,40 @@
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 export default function TodoList() {
-  let [todos, setTodos] = useState(["sample Task"]);
+  let [todos, setTodos] = useState([{ task: "sample Task", id: uuidv4() }]);
   let [newTodo, setNewTodo] = useState("");
 
   let addNewTask = () => {
-    setTodos([...todos, newTodo]);
+    setTodos((prevTodo) => {
+      return [...prevTodo, { task: newTodo, id: uuidv4() }];
+    });
     setNewTodo("");
   };
   let updateTodoValue = (event) => {
     setNewTodo(event.target.value);
   };
 
+  let deleteTodo = (id) => {
+    setTodos((prevTodo) => todos.filter((prevTodo) => prevTodo.id != id));
+  };
+
+  let markAsDone = (id) => {
+    setTodos((prevTodo) =>
+      prevTodo.map((todo) =>
+        todo.id === id ? { ...todo, done: !todo.done } : todo
+      )
+    );
+  };
+
   return (
     <div>
-      <input type="text" placeholder="add a task" value={newTodo} onChange={updateTodoValue} />
+      <input
+        type="text"
+        placeholder="add a task"
+        value={newTodo}
+        onChange={updateTodoValue}
+      />
       <br />
       <button onClick={addNewTask}>Add Task</button>
       <br />
@@ -24,7 +44,20 @@ export default function TodoList() {
       <h4>Todo List</h4>
       <ul>
         {todos.map((todo) => (
-          <li>{todo}</li>
+          <li key={todo.id}>
+            <span
+              style={{
+                textDecoration: todo.done ? "line-through" : "none",
+              }}
+            >
+              {todo.task}
+            </span>
+            &nbsp; &nbsp; &nbsp;
+            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+            <button onClick={() => markAsDone(todo.id)}>
+              {todo.done ? "Undo" : "Mark as done"}
+            </button>
+          </li>
         ))}
       </ul>
     </div>
